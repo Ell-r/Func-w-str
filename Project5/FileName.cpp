0d8574c30c41dc4e8bcf516f3d40588149feccb5
Ell-r <ell.herill.e@gmail.com>
2024-02-07 14:58:43 +0200

    Added func mystrcmp

--- Project5/FileName.cpp
+++ Project5/FileName.cpp
@@ -8,8 +8,40 @@ void fillStr(char str[]) {
 	cin >> str;
 }
 
+int mystrcmp(const char* str1, const char* str2) {
+	int size1 = 0, size2 = 0;
+	for (int i = 0; str1[i] != '\0' || str2[i] != '\0'; i++) {
+		if (str1[i] != str2[i]) {
+			if (str1[i] > str2[i]) {
+				return 1;
+			}
+			else if (str1[i] < str2[i]) {
+				return -1;
+			}
+		}
+	}
+
+	if (strlen(str1) == strlen(str2)) {
+		return 0;
+	}
+
+	else if (strlen(str1) > strlen(str2)) {
+		return 1;
+	}
+
+	else if (strlen(str1) < strlen(str2)) {
+		return -1;
+	}
+
+}
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
+	/*char str1[100], str2[100];
+	fillStr(str1);
+	fillStr(str2);
+	int result = mystrcmp(str1, str2);
+	cout << result;*/
 
 }
